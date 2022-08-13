19e639b9022dc7696135259c341ed29f1218f922
Gui Gomes <gui@guibgomes.com>
2022-08-13 12:34:16 +0200

    Started working on bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/18 19:47:20 by gbraga-g          #+#    #+#             */
+/*   Updated: 2022/07/18 20:00:34 by gbraga-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+char *get_next_line(int fd);
+void *ft_calloc(size_t count, size_t size);
+char *ft_strchr(char *s, int c);
+int ft_linelen(char *s);
+char *ft_strjoin(char*s1, char*s2);
+char *ft_read(int fd, char *buffer);
+char *ft_extract_line(char *buffer);
+size_t ft_strlcpy(char *dst, char *src, size_t dstsize);
+int ft_strlen(char *s);
+char *ft_remove_line(char *buffer);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbraga-g <gbraga-g@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/18 19:47:35 by gbraga-g          #+#    #+#             */
+/*   Updated: 2022/08/13 12:21:33 by gbraga-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int ft_strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int ft_linelen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0' && s[i] != '\n')
+		i++;
+	if (s[i] == '\0')
+		return (i);
+	return (i);
+}
+
+char *ft_strchr(char *s, int c)
+{
+	char newC;
+
+	newC = (char)c;
+	while (*s != '\0' && *s != newC)
+		s++;
+	if (*s == newC)
+		return ((char *)s);
+	return (0);
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+	char *ptr;
+	int i;
+	int j;
+	int len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	ptr = malloc((len + 1) * sizeof(char));
+	if (!ptr || !s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+	ptr[len] = '\0';
+	return (ptr);
+}
+
+void *ft_calloc(size_t count, size_t size)
+{
+	char *ptr;
+	size_t i;
+
+	i = 0;
+	ptr = (char *)malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < count)
+	{
+		*(ptr + i) = '\0';
+		i++;
+	}
+	return (ptr);
+}
+
+size_t ft_strlcpy(char *dst, char *src, size_t dstsize)
+{
+	size_t i;
+	size_t len;
+
+	len = ft_strlen(src);
+	if (dstsize == 0)
+		return (len);
+	i = 0;
+	while (i < dstsize)
+	{
+		if (*(src + i) == '\0')
+		{
+			*(dst + i) = *(src + i);
+			return (len);
+		}
+		*(dst + i) = *(src + i);
+		i++;
+	}
+	*(dst + i - 1) = '\0';
+	return (len);
+}
